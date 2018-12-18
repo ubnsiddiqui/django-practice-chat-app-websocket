6bd5d9a1a8cf1c37b4c0aaac89371bcf11559d51
Muhammad Usama Siddiqui <usiddiqui14@gmail.com>
2018-12-18 14:59:09 +0500

    Test Version

--- chat/views.py
+++ chat/views.py
@@ -3,30 +3,40 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.shortcuts import render
 
-from .models import Message
+from .models import Message, Room
 
 
 # Create your views here.
 @login_required
 def index(request):
     # uid4 = str(uuid.uuid4())
+    user = User.objects.all()
+    room = Room.objects.all()
     chatrooms = Message.objects.all().values_list('group_name', flat=True).distinct()
-    return render(request, 'chat/index.html', {'chatrooms': chatrooms})
+    return render(request, 'chat/index.html', {'chatrooms': chatrooms,'rooms':room,'users':user})
 
 
 @login_required
 def new_room(request, room_name):
     chat_messages = Message.objects.filter(group_name=room_name).order_by("created")[:100]
+    room = Room.objects.filter(name=room_name)
+    user = User.objects.all()
     return render(request, 'chat/room.html', {
         'chat_messages': chat_messages,
-        'room_name': room_name
+        'room_name': room_name,
+        'room': room,
+        'users': user
     })
 
 
 @login_required
 def chat_room(request, room_name):
     chat_messages = Message.objects.filter(group_name=room_name).order_by("created")[:100]
+    room = Room.objects.filter(name=room_name)
+    user = User.objects.all()
     return render(request, 'chat/room.html', {
         'chat_messages': chat_messages,
-        'room_name': room_name
+        'room_name': room_name,
+        'room': room,
+        'users' : user
     })
